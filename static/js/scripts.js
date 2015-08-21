789b533a0562c8e3f23c51ba523e6b545b79b7a0
Jeremy Atkinson <jchatkinson@gmail.com>
2015-08-20 23:37:45 -0700

    sidenav buttons screwy, but home page is working

---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // Pushpin (sticky) box of social links
+    $('.social-box').pushpin({ top: $("main").offset().top });
+    
+    
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+    
+  });
